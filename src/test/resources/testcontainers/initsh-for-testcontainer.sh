0f671c55363e013f1d4010bb39dc8e02b678a5cd
Adrian,Borta <adrian.borta@accenture.com>
2025-03-04 17:55:43 +0100

    feat(PN-14065): update init.sh

--- src/test/resources/testcontainers/initsh-for-testcontainer.sh
+++ src/test/resources/testcontainers/initsh-for-testcontainer.sh
@@ -1,3 +1,3 @@
 echo "### CREATE AWS OBJECTS FOR IT TESTS ###"
 
-bash <(curl -s https://raw.githubusercontent.com/pagopa/pn-portfat/0ee1c230ad8d6fa0939c1a57b2c83df83a910016/src/test/resources/testcontainers/init.sh)
+bash <(curl -s https://raw.githubusercontent.com/pagopa/pn-portfat/9fa9194f216bfb479dfaeb81fd78bd2f5de8fdb5/src/test/resources/testcontainers/init.sh)
