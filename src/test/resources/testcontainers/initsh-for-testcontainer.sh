50120d875fe25c2400f62324ab5be9eb1f264244
Adrian,Borta <adrian.borta@accenture.com>
2025-03-05 16:39:23 +0100

    feat(PN-14067): puntamento a init.sh

--- src/test/resources/testcontainers/initsh-for-testcontainer.sh
+++ src/test/resources/testcontainers/initsh-for-testcontainer.sh
@@ -1,3 +1,3 @@
 echo "### CREATE AWS OBJECTS FOR IT TESTS ###"
 
-bash <(curl -s https://raw.githubusercontent.com/pagopa/pn-portfat/9fa9194f216bfb479dfaeb81fd78bd2f5de8fdb5/src/test/resources/testcontainers/init.sh)
+bash <(curl -s https://raw.githubusercontent.com/pagopa/pn-portfat/15675fdb56b6cdac78310623863b94b8f421cf15/src/test/resources/testcontainers/init.sh)
