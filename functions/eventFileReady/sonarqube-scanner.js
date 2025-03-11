5c645ab2ca40950856fb276522b5dc5c83ea2ed0
pasqualino-cristaudo-acn <pasqualino.cristaudo@accenture.com>
2025-03-11 19:44:58 +0100

    feature(config): updated sonarqube

--- functions/eventFileReady/sonarqube-scanner.js
+++ functions/eventFileReady/sonarqube-scanner.js
@@ -1,37 +1,37 @@
-const scanner = require("sonarqube-scanner");
-
-const options = {
-    "sonar.organization": "pagopa",
-    "sonar.projectKey": "pagopa_pn-portfat-EventFileReady",
-};
-
-if (process.env.PR_NUM) {
-    options["sonar.pullrequest.base"] = process.env.BRANCH_TARGET;
-    options["sonar.pullrequest.branch"] = process.env.BRANCH_NAME;
-    options["sonar.pullrequest.key"] = process.env.PR_NUM;
-}
-
-(async () => {
-    try {
-        await new Promise((resolve, reject) => {
-            scanner(
-                {
-                    serverUrl: "https://sonarcloud.io",
-                    options: options,
-                },
-                (error) => {
-                    if (error) {
-                        reject(error);
-                    } else {
-                        console.log("SonarQube Scanner execution completed.");
-                        resolve();
-                    }
-                }
-            );
-        });
-        process.exit(0);
-    } catch (error) {
-        console.error("SonarQube Scanner failed:", error);
-        process.exit(1);
-    }
-})();
+// const scanner = require("sonarqube-scanner");
+//
+// const options = {
+//     "sonar.organization": "pagopa",
+//     "sonar.projectKey": "pagopa_pn-portfat-EventFileReady",
+// };
+//
+// if (process.env.PR_NUM) {
+//     options["sonar.pullrequest.base"] = process.env.BRANCH_TARGET;
+//     options["sonar.pullrequest.branch"] = process.env.BRANCH_NAME;
+//     options["sonar.pullrequest.key"] = process.env.PR_NUM;
+// }
+//
+// (async () => {
+//     try {
+//         await new Promise((resolve, reject) => {
+//             scanner(
+//                 {
+//                     serverUrl: "https://sonarcloud.io",
+//                     options: options,
+//                 },
+//                 (error) => {
+//                     if (error) {
+//                         reject(error);
+//                     } else {
+//                         console.log("SonarQube Scanner execution completed.");
+//                         resolve();
+//                     }
+//                 }
+//             );
+//         });
+//         process.exit(0);
+//     } catch (error) {
+//         console.error("SonarQube Scanner failed:", error);
+//         process.exit(1);
+//     }
+// })();
