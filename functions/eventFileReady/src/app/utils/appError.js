0cd4937b75442a71c6404a5c5352ac513fbaeb0b
pasqualino-cristaudo-acn <pasqualino.cristaudo@accenture.com>
2025-03-11 17:56:11 +0100

    feature(error): added custom error

---
+++ functions/eventFileReady/src/app/utils/appError.js
@@ -0,0 +1,10 @@
+class AppError extends Error {
+    constructor(statusCode, message, details) {
+        super(message);
+        this.statusCode = statusCode;
+        this.details = details;
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+module.exports = AppError;
