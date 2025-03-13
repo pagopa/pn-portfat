a22e8abea0dce01f45c59a770d35ef0489a76115
pasqualino-cristaudo-acn <pasqualino.cristaudo@accenture.com>
2025-03-13 19:14:37 +0100

    feature(test): updated sqs IT test

--- src/test/java/it/pagopa/pn/portfat/config/BaseTest.java
+++ src/test/java/it/pagopa/pn/portfat/config/BaseTest.java
@@ -2,8 +2,7 @@ package it.pagopa.pn.portfat.config;
 
 import it.pagopa.pn.portfat.LocalStackTestConfig;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,34 +13,38 @@ import org.springframework.test.context.ActiveProfiles;
 @Slf4j
 @SpringBootTest
 @ActiveProfiles("test")
+@Import(LocalStackTestConfig.class)
 @AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public abstract class BaseTest {
 
     @Slf4j
     @SpringBootTest
     @ActiveProfiles("test")
     @Import(LocalStackTestConfig.class)
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
     public static class WithLocalStack {
-
     }
 
     @Slf4j
     @SpringBootTest
     @ActiveProfiles("test")
     @Import(LocalStackTestConfig.class)
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
     public static class WithMockServer {
+
         @Autowired
         private MockServerBean mockServer;
 
-        @BeforeEach
-        void init() {
-            log.info(this.getClass().getSimpleName());
+        @BeforeAll
+        public void init() {
+            log.info("Starting tests in {}", this.getClass().getSimpleName());
             setExpection(this.getClass().getSimpleName() + ".json");
         }
 
-        @AfterEach
-        void kill() {
-            log.info("Killed");
+        @AfterAll
+        public void tearDown() {
+            log.info("Stopping MockServer");
             this.mockServer.stop();
         }
 
@@ -49,6 +52,4 @@ public abstract class BaseTest {
             this.mockServer.initializationExpection(file);
         }
     }
-
-
 }
--- src/test/java/it/pagopa/pn/portfat/middleware/queue/QueueListenerTestIT.java
+++ src/test/java/it/pagopa/pn/portfat/middleware/queue/QueueListenerTestIT.java
@@ -3,28 +3,27 @@ package it.pagopa.pn.portfat.middleware.queue;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.model.CreateQueueRequest;
 import com.amazonaws.services.sqs.model.SendMessageRequest;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import it.pagopa.pn.portfat.config.BaseTest;
 import it.pagopa.pn.portfat.generated.openapi.server.v1.dto.FileReadyEvent;
 import it.pagopa.pn.portfat.middleware.db.dao.PortFatDownloadDAO;
 import it.pagopa.pn.portfat.service.PortFatService;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.mock.mockito.SpyBean;
-import org.springframework.web.reactive.function.client.WebClient;
-
+import org.springframework.test.context.TestPropertySource;
+import org.testcontainers.shaded.org.awaitility.Awaitility;
+import reactor.core.publisher.Mono;
 import java.time.Duration;
-
+import java.util.concurrent.atomic.AtomicReference;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.testcontainers.shaded.org.awaitility.Awaitility.await;
+import static org.mockito.Mockito.*;
 
-@Disabled
-class QueueListenerTestIT extends BaseTest.WithMockServer {
+
+@TestPropertySource(properties = { "spring.cloud.aws.sqs.listener.concurrency=1" })
+public class QueueListenerTestIT extends BaseTest.WithMockServer {
 
     @Autowired
     private AmazonSQSAsync amazonSQS;
@@ -38,36 +37,54 @@ class QueueListenerTestIT extends BaseTest.WithMockServer {
     @SpyBean
     private PortFatService portfatService;
 
-    private WebClient webClient;
+    @Value("${pn.portfat.blobStorageBaseUrl}")
+    private String blobStorageBaseUrl;
 
-    private static final int MOCK_SERVER_PORT = 1080;
+    @Value("${pn.portfat.queue}")
+    private String QUEUE_NAME;
+
+    @Value("${pn.portfat.zipIntegrationTest}")
+    private String fileZipIntegrationTest;
 
     private static final String MESSAGE_GROUP_ID = "port-fat_1";
-    private static final String QUEUE_NAME = "local-pn-portfat-inputs-requests.fifo";
-    private String queueUrl;
+
+    private final AtomicReference<Mono<Void>> processingMonoRef = new AtomicReference<>();
+
 
     @BeforeEach
-    void setup() throws JsonProcessingException {
+    void setup() throws Exception {
+        doAnswer(invocation -> {
+            Object result = invocation.callRealMethod();
+            if (result instanceof Mono) {
+                processingMonoRef.set((Mono<Void>) result);
+            }
+            return result;
+        }).when(portfatService).processZipFile(any());
+
         amazonSQS.createQueue(new CreateQueueRequest().withQueueName(QUEUE_NAME));
-        queueUrl = amazonSQS.getQueueUrl(QUEUE_NAME).getQueueUrl();
+        String queueUrl = amazonSQS.getQueueUrl(QUEUE_NAME).getQueueUrl();
 
-        // Crea il messaggio da inviare alla coda
         FileReadyEvent event = new FileReadyEvent();
-        event.setDownloadUrl("https://storage.portalefatturazione.pagopa.it/portfatt/pn-example_it/pn-example_it");
-        event.setFileVersion("v1.0");
-        String messageBody = objectMapper.writeValueAsString(event);
+        event.setDownloadUrl(blobStorageBaseUrl + "/portfatt/invoices/" + fileZipIntegrationTest);
+        event.setFileVersion("1.0");
 
+        String messageBody = objectMapper.writeValueAsString(event);
         SendMessageRequest sendMessageRequest = new SendMessageRequest()
                 .withQueueUrl(queueUrl)
                 .withMessageBody(messageBody)
                 .withMessageGroupId(MESSAGE_GROUP_ID);
         amazonSQS.sendMessage(sendMessageRequest);
-        webClient = WebClient.create("http://localhost:" + MOCK_SERVER_PORT);
     }
 
     @Test
     void testMessageReceptionAndProcessing() {
-        await().atMost(Duration.ofSeconds(10))
-                .untilAsserted(() -> verify(portfatService, times(1)).processZipFile(any()));
+        Awaitility.await()
+                .atMost(Duration.ofSeconds(10))
+                .until(() -> processingMonoRef.get() != null);
+
+
+        verify(portfatService, times(1)).processZipFile(any());
+
+        processingMonoRef.get().block();
     }
 }
