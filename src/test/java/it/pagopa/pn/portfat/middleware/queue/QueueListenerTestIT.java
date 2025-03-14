c83725f1b0c4abcf4ce05ec9aca0f28c9386f583
Adrian,Borta <adrian.borta@accenture.com>
2025-03-14 14:25:20 +0100

    feat(PN-14045): log message non valid

--- src/test/java/it/pagopa/pn/portfat/middleware/queue/QueueListenerTestIT.java
+++ src/test/java/it/pagopa/pn/portfat/middleware/queue/QueueListenerTestIT.java
@@ -5,24 +5,28 @@ import com.amazonaws.services.sqs.model.CreateQueueRequest;
 import com.amazonaws.services.sqs.model.SendMessageRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import it.pagopa.pn.portfat.config.BaseTest;
+import it.pagopa.pn.portfat.config.PortFatPropertiesConfig;
 import it.pagopa.pn.portfat.generated.openapi.server.v1.dto.FileReadyEvent;
 import it.pagopa.pn.portfat.middleware.db.dao.PortFatDownloadDAO;
 import it.pagopa.pn.portfat.service.PortFatService;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.mockserver.client.MockServerClient;
+import org.mockserver.model.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.mock.mockito.SpyBean;
-import org.testcontainers.shaded.org.awaitility.Awaitility;
-import reactor.core.publisher.Mono;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.Duration;
-import java.util.concurrent.atomic.AtomicReference;
+
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
+import static org.testcontainers.shaded.org.awaitility.Awaitility.await;
 
-
-@Disabled
 class QueueListenerTestIT extends BaseTest.WithMockServer {
 
     @Autowired
@@ -31,38 +35,24 @@ class QueueListenerTestIT extends BaseTest.WithMockServer {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @Autowired
+    @SpyBean
     private PortFatDownloadDAO portFatDownloadDAO;
 
     @SpyBean
     private PortFatService portfatService;
 
-    @Value("${pn.portfat.blobStorageBaseUrl}")
-    private String blobStorageBaseUrl;
-
-    @Value("${pn.portfat.queue}")
-    private String QUEUE_NAME;
+    @Autowired
+    private PortFatPropertiesConfig portFatPropertiesConfig;
 
     private static final String MESSAGE_GROUP_ID = "port-fat_1";
-
-    private final AtomicReference<Mono<Void>> processingMonoRef = new AtomicReference<>();
-
+    private final FileReadyEvent event = new FileReadyEvent();
 
     @BeforeEach
     void setup() throws Exception {
-        doAnswer(invocation -> {
-            Object result = invocation.callRealMethod();
-            if (result instanceof Mono) {
-                processingMonoRef.set((Mono<Void>) result);
-            }
-            return result;
-        }).when(portfatService).processZipFile(any());
+        amazonSQS.createQueue(new CreateQueueRequest().withQueueName(portFatPropertiesConfig.getQueue()));
+        String queueUrl = amazonSQS.getQueueUrl(portFatPropertiesConfig.getQueue()).getQueueUrl();
 
-        amazonSQS.createQueue(new CreateQueueRequest().withQueueName(QUEUE_NAME));
-        String queueUrl = amazonSQS.getQueueUrl(QUEUE_NAME).getQueueUrl();
-
-        FileReadyEvent event = new FileReadyEvent();
-        event.setDownloadUrl(blobStorageBaseUrl + "/portfatt/invoices/portFatt.zip");
+        event.setDownloadUrl(portFatPropertiesConfig.getBlobStorageBaseUrl() + "/portfatt/invoices/portFatt.zip");
         event.setFileVersion("1.0");
 
         String messageBody = objectMapper.writeValueAsString(event);
@@ -71,17 +61,23 @@ class QueueListenerTestIT extends BaseTest.WithMockServer {
                 .withMessageBody(messageBody)
                 .withMessageGroupId(MESSAGE_GROUP_ID);
         amazonSQS.sendMessage(sendMessageRequest);
+
+        byte[] zipBytes = Files.readAllBytes(Paths.get("src/test/resources/portFatt.zip"));
+        new MockServerClient("localhost", 1050)
+                .when(request()
+                        .withMethod("GET")
+                        .withPath("/portfatt/invoices/portFatt.zip")
+                )
+                .respond(response()
+                        .withStatusCode(200)
+                        .withContentType(MediaType.APPLICATION_OCTET_STREAM)
+                        .withBody(zipBytes)
+                );
     }
 
     @Test
     void testMessageReceptionAndProcessing() {
-        Awaitility.await()
-                .atMost(Duration.ofSeconds(20))
-                .until(() -> processingMonoRef.get() != null);
-
-
-        verify(portfatService, times(1)).processZipFile(any());
-
-        processingMonoRef.get().block();
+        await().atMost(Duration.ofSeconds(15))
+                .untilAsserted(() -> verify(portfatService, times(1)).processZipFile(any()));
     }
 }
