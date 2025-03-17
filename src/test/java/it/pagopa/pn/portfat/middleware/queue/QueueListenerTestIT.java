9fb85ebc4a97e35d0bda637169171f754c60744a
pasqualino-cristaudo-acn <pasqualino.cristaudo@accenture.com>
2025-03-17 18:06:00 +0100

    feature(IT): updated sqs integration test

--- src/test/java/it/pagopa/pn/portfat/middleware/queue/QueueListenerTestIT.java
+++ src/test/java/it/pagopa/pn/portfat/middleware/queue/QueueListenerTestIT.java
@@ -6,16 +6,14 @@ import com.amazonaws.services.sqs.model.SendMessageRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import it.pagopa.pn.portfat.config.BaseTest;
 import it.pagopa.pn.portfat.config.PortFatPropertiesConfig;
-import it.pagopa.pn.portfat.config.SqsTestConfig;
 import it.pagopa.pn.portfat.generated.openapi.server.v1.dto.FileReadyEvent;
 import it.pagopa.pn.portfat.middleware.db.dao.PortFatDownloadDAO;
-import it.pagopa.pn.portfat.service.PortFatService;
+import it.pagopa.pn.portfat.middleware.db.entities.DownloadStatus;
+import it.pagopa.pn.portfat.middleware.db.entities.PortFatDownload;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.testcontainers.shaded.org.awaitility.Awaitility;
 import java.io.IOException;
@@ -23,18 +21,17 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Duration;
+import java.util.Optional;
 import java.util.stream.Stream;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockserver.model.HttpRequest.request;
 import static org.mockserver.model.HttpResponse.response;
 
 
 @Slf4j
-@SpringBootTest(classes = { SqsTestConfig.class },
-        properties = "spring.autoconfigure.exclude=io.awspring.cloud.autoconfigure.messaging.SqsAutoConfiguration")
 class QueueListenerTestIT extends BaseTest.WithMockServer {
 
     @Autowired
-    @Qualifier("amazonSQSAsync")
     private AmazonSQSAsync amazonSQS;
 
     @Autowired
@@ -42,9 +39,6 @@ class QueueListenerTestIT extends BaseTest.WithMockServer {
 
     @SpyBean
     private PortFatDownloadDAO portFatDownloadDAO;
-
-    @SpyBean
-    private PortFatService portfatService;
 
     @Autowired
     private PortFatPropertiesConfig portFatPropertiesConfig;
@@ -73,15 +67,34 @@ class QueueListenerTestIT extends BaseTest.WithMockServer {
     void testMessageReceptionAndProcessing() throws IOException {
         configMockServer();
         Path baseZipDir = Paths.get(portFatPropertiesConfig.getBasePathZipFiele());
+
         Awaitility.await()
-                .atMost(Duration.ofSeconds(120))
+                .atMost(Duration.ofSeconds(25))
                 .until(() -> {
                     try (Stream<Path> stream = Files.list(baseZipDir)) {
-                        return stream.findAny().isEmpty();
-                    } catch (IOException e) {
-                        return !Files.exists(baseZipDir);
+                        return stream.anyMatch(Files::isDirectory);
                     }
                 });
+
+        Optional<Path> generatedDirOptional = Files.list(baseZipDir)
+                .filter(Files::isDirectory)
+                .findFirst();
+        assertThat(generatedDirOptional).isPresent();
+        Path generatedDir = generatedDirOptional.get();
+
+        Awaitility.await()
+                .atMost(Duration.ofSeconds(5))
+                .until(() -> {
+                    try (Stream<Path> stream = Files.list(generatedDir)) {
+                        return stream.findAny().isEmpty();
+                    } catch (IOException e) {
+                        return !Files.exists(generatedDir);
+                    }
+                });
+
+        Optional<PortFatDownload> download = portFatDownloadDAO.findByDownloadId("http://localhost:1050/portfatt/invoices/portFatt.zip1.0").blockOptional();
+        assertThat(download).isPresent();
+        assertThat(download.get().getStatus()).isEqualTo(DownloadStatus.COMPLETED);
     }
 
 
