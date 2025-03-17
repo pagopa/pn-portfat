a5201bfbd0fc816e48ed9ba22b6b53fde2a4cbb8
pasqualino-cristaudo-acn <pasqualino.cristaudo@accenture.com>
2025-03-17 15:13:07 +0100

    feature(IT): updated sqs integration test

--- src/test/java/it/pagopa/pn/portfat/middleware/queue/QueueListenerTestIT.java
+++ src/test/java/it/pagopa/pn/portfat/middleware/queue/QueueListenerTestIT.java
@@ -6,29 +6,35 @@ import com.amazonaws.services.sqs.model.SendMessageRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import it.pagopa.pn.portfat.config.BaseTest;
 import it.pagopa.pn.portfat.config.PortFatPropertiesConfig;
+import it.pagopa.pn.portfat.config.SqsTestConfig;
 import it.pagopa.pn.portfat.generated.openapi.server.v1.dto.FileReadyEvent;
 import it.pagopa.pn.portfat.middleware.db.dao.PortFatDownloadDAO;
 import it.pagopa.pn.portfat.service.PortFatService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.testcontainers.shaded.org.awaitility.Awaitility;
-import reactor.core.publisher.Mono;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Duration;
-import java.util.concurrent.atomic.AtomicReference;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import java.util.stream.Stream;
 import static org.mockserver.model.HttpRequest.request;
 import static org.mockserver.model.HttpResponse.response;
 
 
+@Slf4j
+@SpringBootTest(classes = { SqsTestConfig.class },
+        properties = "spring.autoconfigure.exclude=io.awspring.cloud.autoconfigure.messaging.SqsAutoConfiguration")
 class QueueListenerTestIT extends BaseTest.WithMockServer {
 
     @Autowired
+    @Qualifier("amazonSQSAsync")
     private AmazonSQSAsync amazonSQS;
 
     @Autowired
@@ -45,22 +51,9 @@ class QueueListenerTestIT extends BaseTest.WithMockServer {
 
     private static final String MESSAGE_GROUP_ID = "port-fat_1";
 
-    private final AtomicReference<Mono<Void>> processingMonoRef = new AtomicReference<>();
-
 
     @BeforeEach
     void setup() throws Exception {
-        doAnswer(invocation -> {
-            Object result = invocation.callRealMethod();
-            if (result instanceof Mono) {
-                if (processingMonoRef.get() == null) {
-                    processingMonoRef.set(Mono.from((Mono<?>) result).then());
-                }
-                return result;
-            }
-            return result;
-        }).when(portfatService).processZipFile(any());
-
         amazonSQS.createQueue(new CreateQueueRequest().withQueueName(portFatPropertiesConfig.getSqsQueue()));
         String queueUrl = amazonSQS.getQueueUrl(portFatPropertiesConfig.getSqsQueue()).getQueueUrl();
 
@@ -79,14 +72,16 @@ class QueueListenerTestIT extends BaseTest.WithMockServer {
     @Test
     void testMessageReceptionAndProcessing() throws IOException {
         configMockServer();
-
+        Path baseZipDir = Paths.get(portFatPropertiesConfig.getBasePathZipFiele());
         Awaitility.await()
-                .atMost(Duration.ofSeconds(60))
-                .until(() -> processingMonoRef.get() != null);
-
-        verify(portfatService, times(1)).processZipFile(any());
-
-        processingMonoRef.get().block();
+                .atMost(Duration.ofSeconds(120))
+                .until(() -> {
+                    try (Stream<Path> stream = Files.list(baseZipDir)) {
+                        return stream.findAny().isEmpty();
+                    } catch (IOException e) {
+                        return !Files.exists(baseZipDir);
+                    }
+                });
     }
 
 
