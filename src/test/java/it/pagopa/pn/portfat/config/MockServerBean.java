69f673fa00a2413b3a4460e5564fee2ec1ef9ca7
pasqualino-cristaudo-acn <pasqualino.cristaudo@accenture.com>
2025-03-19 09:52:06 +0100

    feature(IT): updated test

--- src/test/java/it/pagopa/pn/portfat/config/MockServerBean.java
+++ src/test/java/it/pagopa/pn/portfat/config/MockServerBean.java
@@ -4,24 +4,34 @@ import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.mockserver.configuration.ConfigurationProperties;
 import org.mockserver.integration.ClientAndServer;
+import org.mockserver.model.HttpResponse;
+import org.mockserver.model.RequestDefinition;
+import org.mockserver.verify.VerificationTimes;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import java.io.IOException;
 
 
 @Slf4j
+@Getter
 public class MockServerBean {
-    @Getter
     private ClientAndServer mockServer;
     private final int port;
 
-    public MockServerBean(int port){
+    public MockServerBean(int port) {
         this.port = port;
-        log.info("Mock server started on : {}", port);
     }
 
-    public void stop(){
-        this.mockServer.stop();
+    public void stop() {
+        try {
+            if (this.mockServer != null && this.mockServer.isRunning()) {
+                this.mockServer.reset();
+            }
+        } finally {
+            assert this.mockServer != null;
+            this.mockServer.stop();
+            log.info("Mock server stopped on: {}", mockServer.getPort());
+        }
     }
 
     public void initializationExpection(String file){
@@ -32,8 +42,17 @@ public class MockServerBean {
             log.info(" - Path : {} ", path);
             ConfigurationProperties.initializationJsonPath(path);
             this.mockServer = ClientAndServer.startClientAndServer(port);
+            log.info("Mock server started on: {}", port);
         } catch (IOException e) {
             log.error(" - File json not found");
         }
+    }
+
+    public void setRequestResponse(RequestDefinition requestDefinition, HttpResponse httpResponse) {
+        this.mockServer.when(requestDefinition).respond(httpResponse);
+    }
+
+    public void toVerify(RequestDefinition requestDefinition, VerificationTimes times) {
+        this.mockServer.verify(requestDefinition, times);
     }
 }
--- src/test/java/it/pagopa/pn/portfat/middleware/queue/QueueListenerTestIT.java
+++ src/test/java/it/pagopa/pn/portfat/middleware/queue/QueueListenerTestIT.java
@@ -1,7 +1,9 @@
 package it.pagopa.pn.portfat.middleware.queue;
 
 import com.amazonaws.services.sqs.AmazonSQSAsync;
-import com.amazonaws.services.sqs.model.CreateQueueRequest;
+import com.amazonaws.services.sqs.model.DeleteMessageRequest;
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
 import com.amazonaws.services.sqs.model.SendMessageRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import it.pagopa.pn.portfat.config.BaseTest;
@@ -11,26 +13,29 @@ import it.pagopa.pn.portfat.middleware.db.dao.PortFatDownloadDAO;
 import it.pagopa.pn.portfat.middleware.db.entities.DownloadStatus;
 import it.pagopa.pn.portfat.middleware.db.entities.PortFatDownload;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockserver.verify.VerificationTimes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.SpyBean;
-import org.springframework.test.annotation.DirtiesContext;
 import org.testcontainers.shaded.org.awaitility.Awaitility;
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Duration;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockserver.model.HttpRequest.request;
 import static org.mockserver.model.HttpResponse.response;
 
 
 @Slf4j
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 class QueueListenerTestIT extends BaseTest.WithMockServer {
 
     @Autowired
@@ -45,80 +50,110 @@ class QueueListenerTestIT extends BaseTest.WithMockServer {
     @Autowired
     private PortFatPropertiesConfig portFatPropertiesConfig;
 
-    private static final String MESSAGE_GROUP_ID = "port-fat_1";
+    private final String filePath = "/portfatt/invoices/portFatt.zip";
 
-    private final String fileVersion = "1.0";
     private String fileUrl;
 
-    @BeforeEach
-    void setup() throws Exception {
-        String filePath = "/portfatt/invoices/portFatt.zip";
+
+    @PostConstruct
+    public void setup() {
         fileUrl = portFatPropertiesConfig.getBlobStorageBaseUrl() + filePath;
+    }
 
-        amazonSQS.createQueue(new CreateQueueRequest().withQueueName(portFatPropertiesConfig.getSqsQueue()));
+
+    @BeforeEach
+    void cleanBefore() {
+        clearSqsQueue();
+    }
+
+    @AfterEach
+    void cleanAfter() {
+        clearSqsQueue();
+        tearDown();
+    }
+
+    @Test
+    void testMessageDequeuedAndProcessing() throws IOException {
+        String fileVersion = "1.0";
+        String messageGroupId = "port-fat_1";
+        configMockServerMessageDequeuedAndProcessing();
+        pushOnQueue(fileVersion, messageGroupId);
+        Path baseZipDir = Paths.get(portFatPropertiesConfig.getBasePathZipFile());
+
+        Awaitility.await()
+                .atMost(Duration.ofSeconds(40))
+                .until(() -> {
+                    PortFatDownload download = portFatDownloadDAO
+                            .findByDownloadId(fileUrl + fileVersion)
+                            .block();
+                    return download != null && download.getStatus() == DownloadStatus.COMPLETED;
+                });
+
+        Awaitility.await()
+                .atMost(Duration.ofSeconds(25))
+                .until(() -> {
+                    try (Stream<Path> stream = Files.list(baseZipDir)) {
+                        return stream.noneMatch(Files::isDirectory);
+                    } catch (IOException e) {
+                        return true;
+                    }
+                });
+
+        Optional<PortFatDownload> download = portFatDownloadDAO
+                .findByDownloadId(fileUrl + fileVersion)
+                .blockOptional();
+        assertThat(download).isPresent();
+        assertThat(download.get().getStatus()).isEqualTo(DownloadStatus.COMPLETED);
+
         String queueUrl = amazonSQS.getQueueUrl(portFatPropertiesConfig.getSqsQueue()).getQueueUrl();
+        ReceiveMessageRequest request = new ReceiveMessageRequest()
+                .withQueueUrl(queueUrl)
+                .withMaxNumberOfMessages(1)
+                .withWaitTimeSeconds(5);
 
+        List<Message> messages = amazonSQS.receiveMessage(request).getMessages();
+        assertTrue(messages.isEmpty());
+
+        getMockServerBean().toVerify(request().withMethod("GET").withPath(filePath), VerificationTimes.atLeast(1));
+        getMockServerBean().toVerify(request().withMethod("POST").withPath("/safe-storage/v1/files"), VerificationTimes.atLeast(1));
+        getMockServerBean().toVerify(request().withMethod("PUT").withPath("/safe-storage/storage/invoice.json"), VerificationTimes.atLeast(1));
+    }
+
+
+    private void pushOnQueue(String fileVersion, String messageGroupId) throws IOException {
+        String queueUrl = amazonSQS.getQueueUrl(portFatPropertiesConfig.getSqsQueue()).getQueueUrl();
         FileReadyEvent event = new FileReadyEvent();
         event.setDownloadUrl(fileUrl);
         event.setFileVersion(fileVersion);
-
         String messageBody = objectMapper.writeValueAsString(event);
         SendMessageRequest sendMessageRequest = new SendMessageRequest()
                 .withQueueUrl(queueUrl)
                 .withMessageBody(messageBody)
-                .withMessageGroupId(MESSAGE_GROUP_ID);
+                .withMessageGroupId(messageGroupId);
         amazonSQS.sendMessage(sendMessageRequest);
     }
 
-    @Test
-    void testMessageReceptionAndProcessing() throws IOException {
-        configMockServer();
-        Path baseZipDir = Paths.get(portFatPropertiesConfig.getBasePathZipFile());
+    void clearSqsQueue() {
+        String queueUrl = amazonSQS.getQueueUrl(portFatPropertiesConfig.getSqsQueue()).getQueueUrl();
+        List<Message> messages = amazonSQS.receiveMessage(
+                new ReceiveMessageRequest()
+                        .withQueueUrl(queueUrl)
+                        .withMaxNumberOfMessages(10)
+                        .withWaitTimeSeconds(1)
+        ).getMessages();
 
-        try {
-            Awaitility.await()
-                    .atMost(Duration.ofSeconds(40))
-                    .until(() -> {
-                        PortFatDownload download = portFatDownloadDAO
-                                .findByDownloadId(fileUrl + fileVersion)
-                                .block();
-                        return download != null && download.getStatus() == DownloadStatus.COMPLETED;
-                    });
-
-            Awaitility.await()
-                    .atMost(Duration.ofSeconds(25))
-                    .until(() -> {
-                        try (Stream<Path> stream = Files.list(baseZipDir)) {
-                            return stream.noneMatch(Files::isDirectory);
-                        } catch (IOException e) {
-                            return true;
-                        }
-                    });
-
-            Optional<PortFatDownload> download = portFatDownloadDAO
-                    .findByDownloadId(fileUrl + fileVersion)
-                    .blockOptional();
-            assertThat(download).isPresent();
-            assertThat(download.get().getStatus()).isEqualTo(DownloadStatus.COMPLETED);
-        } finally {
-            super.tearDown();
+        for (Message message : messages) {
+            amazonSQS.deleteMessage(new DeleteMessageRequest()
+                    .withQueueUrl(queueUrl)
+                    .withReceiptHandle(message.getReceiptHandle()));
         }
     }
 
-
-    private void configMockServer() throws IOException {
+    private void configMockServerMessageDequeuedAndProcessing() throws IOException {
         byte[] zipBytes = Files.readAllBytes(Paths.get("src/test/resources/portFatt.zip"));
-        super.getMockServerBean()
-                .getMockServer()
-                .when(request()
-                        .withMethod("GET")
-                        .withPath("/portfatt/invoices/portFatt.zip")
-                )
-                .respond(response()
-                        .withStatusCode(200)
-                        .withHeader("Content-Type", "application/x-zip-compressed")
-                        .withHeader("Content-Disposition", "attachment; filename=\"file.zip\"")
-                        .withBody(zipBytes)
-                );
+        getMockServerBean().setRequestResponse(
+                request().withMethod("GET").withPath(filePath),
+                response().withStatusCode(200).withHeader("Content-Type", "application/x-zip-compressed").withHeader("Content-Disposition", "attachment; filename=\"file.zip\"").withBody(zipBytes)
+        );
     }
 }
