c08168b1b5b5ce84a5aa959ad26634637a088e7b
Adrian,Borta <adrian.borta@accenture.com>
2025-03-28 11:21:14 +0100

    feat(DOC): add docs

--- src/main/java/it/pagopa/pn/portfat/middleware/db/dao/impl/PortFatDownloadDAOImpl.java
+++ src/main/java/it/pagopa/pn/portfat/middleware/db/dao/impl/PortFatDownloadDAOImpl.java
@@ -10,26 +10,55 @@ import reactor.core.publisher.Mono;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedAsyncClient;
 import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient;
 
+/**
+ * Implementazione del Data Access Object (DAO) per la gestione delle operazioni su PortFatDownload.
+ * Questa classe utilizza DynamoDB come database per memorizzare e recuperare informazioni sui download.
+ */
 @Slf4j
 @Repository
 public class PortFatDownloadDAOImpl extends BaseDAO<PortFatDownload> implements PortFatDownloadDAO {
 
+    /**
+     * Costruttore che inizializza il DAO con il client DynamoDB e la configurazione AWS.
+     *
+     * @param dynamoDbEnhancedAsyncClient client per operazioni asincrone su DynamoDB
+     * @param dynamoDbAsyncClient         client asincrono per operazioni di basso livello su DynamoDB
+     * @param awsPropertiesConfig         configurazione AWS contenente il nome della tabella DynamoDB
+     */
     protected PortFatDownloadDAOImpl(DynamoDbEnhancedAsyncClient dynamoDbEnhancedAsyncClient,
                                      DynamoDbAsyncClient dynamoDbAsyncClient,
                                      AwsPropertiesConfig awsPropertiesConfig) {
         super(dynamoDbEnhancedAsyncClient, dynamoDbAsyncClient, awsPropertiesConfig.getDynamodbPrtFatTable(), PortFatDownload.class);
     }
 
+    /**
+     * Recupera un'istanza di PortFatDownload dal database a partire dal downloadId.
+     *
+     * @param downloadId identificativo del download da cercare
+     * @return un oggetto Mono contenente l'istanza di PortFatDownload, se trovata
+     */
     @Override
     public Mono<PortFatDownload> findByDownloadId(String downloadId) {
         return Mono.fromFuture(this.get(downloadId, null).toFuture());
     }
 
+    /**
+     * Salva una nuova istanza di PortFatDownload nel database.
+     *
+     * @param portFatDownload l'istanza da salvare
+     * @return un oggetto Mono contenente l'istanza salvata
+     */
     @Override
     public Mono<PortFatDownload> createPortFatDownload(PortFatDownload portFatDownload) {
         return Mono.fromFuture(this.put(portFatDownload).toFuture());
     }
 
+    /**
+     * Aggiorna un'istanza esistente di PortFatDownload nel database.
+     *
+     * @param portFatDownload l'istanza da aggiornare con i nuovi dati
+     * @return un oggetto Mono contenente l'istanza aggiornata
+     */
     @Override
     public Mono<PortFatDownload> updatePortFatDownload(PortFatDownload portFatDownload) {
         return Mono.fromFuture(this.update(portFatDownload).toFuture());
--- src/main/java/it/pagopa/pn/portfat/utils/ZipUtility.java
+++ src/main/java/it/pagopa/pn/portfat/utils/ZipUtility.java
@@ -17,13 +17,28 @@ import java.util.zip.ZipFile;
 import static it.pagopa.pn.portfat.exception.ExceptionTypeEnum.JSONS_NOT_FOND_IN_ZIP;
 import static it.pagopa.pn.portfat.exception.ExceptionTypeEnum.ZIP_ERROR;
 
+/**
+ * Classe di utilità per la gestione e l'estrazione di file ZIP.
+ * Fornisce metodi per decomprimere file ZIP e estrarre file JSON in una destinazione specificata.
+ */
 @Slf4j
 public class ZipUtility {
 
+    /**
+     * Costruttore privato per impedire l'istanza della classe. Lancia un'eccezione se chiamato.
+     */
     private ZipUtility() {
         throw new IllegalCallerException();
     }
 
+    /**
+     * Estrae un file ZIP in una directory di destinazione specificata.
+     *
+     * @param zipFilePath   il percorso del file ZIP da estrarre
+     * @param destDirectory la directory di destinazione per i file estratti
+     * @return un Mono vuoto che rappresenta l'operazione asincrona di estrazione
+     * @throws PnGenericException se si verifica un errore durante l'estrazione
+     */
     public static Mono<Void> unzip(String zipFilePath, String destDirectory) {
         return Mono.fromCallable(() -> {
             prepareDestinationDirectory(destDirectory);
@@ -31,6 +46,12 @@ public class ZipUtility {
         }).then();
     }
 
+    /**
+     * Verifica la validità della directory di destinazione.
+     *
+     * @param destDirectory la directory di destinazione da preparare
+     * @throws PnGenericException se non è possibile creare la directory di destinazione
+     */
     private static void prepareDestinationDirectory(String destDirectory) {
         File destDir = new File(destDirectory);
         if (!destDir.exists() && !destDir.mkdirs()) {
@@ -38,6 +59,14 @@ public class ZipUtility {
         }
     }
 
+    /**
+     * Processa il file ZIP, estraendo i file JSON contenuti al suo interno nella directory di destinazione.
+     *
+     * @param zipFilePath   il percorso del file ZIP da estrarre
+     * @param destDirectory la directory di destinazione per i file estratti
+     * @return un valore void dopo aver completato l'estrazione
+     * @throws PnGenericException se si verifica un errore durante il processo del file ZIP
+     */
     private static Void processZipFile(String zipFilePath, String destDirectory) {
         try (ZipFile zipFile = new ZipFile(new File(zipFilePath))) {
             boolean foundJson = extractJsonFiles(zipFile, destDirectory);
@@ -50,6 +79,14 @@ public class ZipUtility {
         return null;
     }
 
+    /**
+     * Estrae i file JSON dal file ZIP e li salva nella directory di destinazione.
+     *
+     * @param zipFile       il file ZIP da cui estrarre i file
+     * @param destDirectory la directory di destinazione per i file estratti
+     * @return true se sono stati trovati e estratti file JSON, altrimenti false
+     * @throws IOException se si verifica un errore durante l'estrazione dei file
+     */
     private static boolean extractJsonFiles(ZipFile zipFile, String destDirectory) throws IOException {
         boolean foundJson = false;
         Path destDirPath = Paths.get(destDirectory).toAbsolutePath().normalize();
@@ -68,19 +105,40 @@ public class ZipUtility {
         return foundJson;
     }
 
+    /**
+     * Salva il contenuto nella directory di destinazione.
+     *
+     * @param zipFile       il file ZIP da cui estrarre la voce
+     * @param zipEntry      la voce ZIP da estrarre
+     * @param destDirectory la directory di destinazione per il file estratto
+     * @throws IOException se si verifica un errore durante il salvataggio del file estratto
+     */
     private static void saveZipEntry(ZipFile zipFile, ZipEntry zipEntry, String destDirectory) throws IOException {
         File destFile = new File(destDirectory, sanitizeEntryName(zipEntry.getName()));
         writeFileContent(zipFile, zipEntry, destFile);
         log.info("Extracted JSON: {}", destFile.getName());
     }
 
+    /**
+     * Sanifica il nome di una voce ZIP per evitare percorsi non sicuri.
+     *
+     * @param entryName il nome della voce ZIP da sanificare
+     * @return il nome sanificato della voce ZIP
+     */
     private static String sanitizeEntryName(String entryName) {
         return entryName.replace("..", "")
                 .replace("/", File.separator)
                 .replace("\\", File.separator);
     }
 
-
+    /**
+     * Scrive il contenuto di una voce ZIP estratta in un file di destinazione.
+     *
+     * @param zipFile  il file ZIP da cui estrarre il contenuto
+     * @param zipEntry la voce ZIP contenente il contenuto da scrivere
+     * @param destFile il file di destinazione in cui scrivere il contenuto
+     * @throws IOException se si verifica un errore durante la scrittura del file
+     */
     private static void writeFileContent(ZipFile zipFile, ZipEntry zipEntry, File destFile) throws IOException {
         File file = new File(destFile, zipEntry.getName());
         if (!file.toPath().normalize().startsWith(destFile.toPath())) {
