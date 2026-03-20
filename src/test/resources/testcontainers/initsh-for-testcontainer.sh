f56db96658dcbced9c4a518b7932bdae3615ba15
marziapirozzi <Marzia.Pirozzi@emeal.nttdata.com>
2026-03-20 18:25:44 +0100

    Update commit id in initsh-for-testcontainer.sh

--- src/test/resources/testcontainers/initsh-for-testcontainer.sh
+++ src/test/resources/testcontainers/initsh-for-testcontainer.sh
@@ -1,3 +1,3 @@
 echo "### CREATE AWS OBJECTS FOR IT TESTS ###"
 
-bash <(curl -s https://raw.githubusercontent.com/pagopa/pn-portfat/15675fdb56b6cdac78310623863b94b8f421cf15/src/test/resources/testcontainers/init.sh)
+bash <(curl -s https://raw.githubusercontent.com/pagopa/pn-portfat/69c699911daecbde1b01d725e7692860f7f796bf/src/test/resources/testcontainers/init.sh)
